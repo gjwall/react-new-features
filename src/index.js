171d40bd61709b56ca21e39eb567eac4379ee261
Graham Wallace <grahamjwallace@gmail.com>
2020-12-12 21:23:59 +0000

    After the useState hook

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
+const App = (props) => {
+
+  const [ count, setCount ] = useState(props.count);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div>
+      <p>The current count is {count}</p>
+      <button onClick={increment}>+1</button>
+      <button onClick={() => setCount(count + 1)}>+1 again</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+App.defaultProps = {
+  count: 0
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <div>My New content</div>
+    <App count={0} />
   </React.StrictMode>,
   document.getElementById('root')
 );
