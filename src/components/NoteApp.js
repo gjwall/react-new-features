6c56de5c67725332e07478c2d71b9ebc4830f1df
Graham Wallace <grahamjwallace@gmail.com>
2020-12-17 14:09:15 +0000

    After The Context API and UseContext Hook part 2

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -2,15 +2,12 @@ import React, { useEffect, useReducer } from 'react';
 import notesReducer from './../reducers/notes';
 import NoteList from './NoteList';
 import AddNoteForm from './AddNoteForm';
+import NotesContext from './../context/notes-context';
 
 const NoteApp = () => {
 
     // Pass the reducer and the initial state
     const [notes, dispatch] = useReducer(notesReducer, [] );
-    
-    const removeNote = (title) => {
-      dispatch( { type: 'REMOVE_NOTE', title } );
-    };
   
     // Use this pattern if fetching data from a database. Use a promise or a callback!
     useEffect( () => {
@@ -25,11 +22,11 @@ const NoteApp = () => {
     }, [notes] );
     
     return (
-      <div>
+      <NotesContext.Provider value={{ notes, dispatch }}>
         <h1>Notes</h1> 
-        <NoteList notes={notes} removeNote={removeNote} /> 
-        <AddNoteForm dispatch={dispatch} />
-      </div>
+        <NoteList /> 
+        <AddNoteForm />
+      </NotesContext.Provider>
     );
   
 };
